41945dd70d378bc6fdd577295ca272af3011e1a6
Yunzhe <iLoveLive@honoka.moe>
2017-12-22 12:32:54 +0800

    First commit.

---
+++ draw.h
@@ -0,0 +1,15 @@
+//
+// Created by Yunzhe on 2017/12/4.
+//
+
+#ifndef GPUBASEDRENDERING_A1_DRAW_H
+#define GPUBASEDRENDERING_A1_DRAW_H
+
+// include global.h
+#include "global.h"
+
+void DrawScene();
+void drawLocator(GLfloat* center, GLfloat radius);
+void drawCrosshair();
+
+#endif //GPUBASEDRENDERING_A1_DRAW_H
---
+++ system.h
@@ -0,0 +1,40 @@
+//
+// Created by Yunzhe on 2017/12/4.
+//
+
+#ifndef GPUBASEDRENDERING_A1_SYSTEM_H
+#define GPUBASEDRENDERING_A1_SYSTEM_H
+
+// Include related head files
+#include "global.h"
+#include "utility.h"
+#include "draw.h"
+#include "Shader.h"
+#include "vbotorus.h"
+#include "vboteapot.h"
+#include "vboplane.h"
+
+// Using namespace std for cout
+using namespace std;
+
+extern Shader shader;
+
+void Idle();
+void Redraw();
+void Reshape(int width, int height);
+void ProcessMouseClick(int button, int state, int x, int y);
+void ProcessMouseMove(int x, int y);
+void ProcessFocus(int state);
+void ProcessNormalKey(unsigned char k, int x, int y);
+void PrintStatus();
+void initVBO();
+void setShader();
+void updateMVPZero();
+void updateMVPOne();
+void updateMVPTwo();
+void updateMVPThree();
+void updateShaderMVP();
+void setupFBO();
+void setupVAO();
+
+#endif //GPUBASEDRENDERING_A1_SYSTEM_H
